e922c8b8174f378707c9bcc67c32f5d3d4694b2c
admin <admin>
2019-11-11 20:20:49 +0000

    Export: commit

---
+++ IGUANA_CHANNEL_VERSION-6_1_0/shared/properties.lua
@@ -0,0 +1,10 @@
+local Properties =  {}
+
+function Properties.directory_path()
+
+  input_directory_path  = "C:\\3PL_WO\\OrderFiles\\"
+  output_archived_path  = "C:\\3PL_WO\\ArchivedFiles\\"
+  output_error_path = "C:\\3PL_WO\\ErrorFiles\\"
+end
+
+return Properties
